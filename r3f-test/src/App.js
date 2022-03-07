f389f790ae48f8fe1ddbebfa3cffe8f4ef51eeab
Dave <thecothineal@gmail.com>
2022-03-07 20:06:22 +0100

    *trying out fiber

--- r3f-test/src/App.js
+++ r3f-test/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import * as THREE from 'three';
+import React, { useRef, Suspense } from 'react';
+import { Canvas, extend, useFrame } from '@react-three/fiber';
+import { shaderMaterial } from '@react-three/drei';
+import glsl from 'babel-plugin-glsl/macro';
 import './App.css';
 
-function App() {
+const WaveShaderMaterial = shaderMaterial(
+  // Check docs for details on each
+  // Uniform
+  {},
+  // Vertex Shader
+  glsl`
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4
+      (position, 1.0);
+    }
+  `,
+  // Fragment Shader
+  glsl`
+    void main() {
+      gl_FragColor = vec4(0.0, 0.4, 1.0, 1.0);
+    }
+  `
+);
+
+extend({ WaveShaderMaterial });
+
+const Scene = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <pointLight position={[10, 10, 10]} />
+      <mesh>
+        <planeBufferGeometry args={[3, 5]} />
+        <meshStandardMaterial color='lightblue' />
+      </mesh>
+    </Canvas>
   );
-}
+};
+
+const App = () => {
+  return <Scene />;
+};
 
 export default App;
