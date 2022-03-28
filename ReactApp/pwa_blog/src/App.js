640897b9aa372baf4b4e5d20907c4c506aeb9081
Dave <thecothineal@gmail.com>
2022-03-28 13:38:53 +0200

    *

--- ReactApp/pwa_blog/src/App.js
+++ ReactApp/pwa_blog/src/App.js
@@ -14,7 +14,6 @@ function App() {
       await fetchData(query);
     }
   }
-  // music aakash gandhi - liquid time?? (not autoplay)
   // input radiobtns OR / AND conn tween qwords
   return (
     <div className="App">
@@ -25,6 +24,15 @@ function App() {
           placeholder='Type something here...'
           onKeyDown={search}
         />
+        <div className='opt'>
+          <input type="radio" id='optOr' name='relation'></input>
+          <label form='optOr'>OR</label>
+          <input type="radio" id='optAnd' name='relation'></input>
+          <label form='optAnd'>AND</label>
+        </div>
+        <audio controls loop="true">
+          <source src='./dan_bodan_-_fortress_europe.mp3' type='audio/mpeg'></source>
+        </audio>
       </div>
       <div className="container"></div>
       <img className='yoda' src='./yoda.gif' alt='yoda'></img>
