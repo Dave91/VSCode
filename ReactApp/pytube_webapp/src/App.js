b24e2284fbffde6f7e75d8d21887fb2c26f6a47f
Dave <thecothineal@gmail.com>
2022-04-11 16:02:51 +0200

    *yt downloader is under dev

--- ReactApp/pytube_webapp/src/App.js
+++ ReactApp/pytube_webapp/src/App.js
@@ -1,103 +1,44 @@
 import "./App.css";
-import SearchBar from "./SearchBar";
-import ItemsDisplay from "./ItemsDisplay";
-import AddItem from "./AddItem";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-	const [filters, setFilters] = useState({});
-	const [data, setData] = useState({ items: [] });
-
-  useEffect(() => {
-    fetch("http://localhost:3000/items")
-    .then((response) => response.json())
-    .then((data) => setData({ items: data }));
-  }, []);
-
-	const updateFilters = (searchParams) => {
-		setFilters(searchParams);
-	};
-
-  const deleteItem = (item) => {
-    const items = data["items"];
-    const requestOptions = {
-      method: "DELETE"
-    }
-    fetch(`http://localhost:3000/items/${item.id}`, requestOptions)
-      .then((response) => {
-        if (response.ok) {
-          const idx = items.indexOf(item);
-          items.splice(idx, 1);
-          setData({ items: items });
-        }
-      }
-    );
+  const UrlInput = document.getElementById("url-input");
+  const UrlImg = document.getElementById("url-img");
+  const [yturl, setYturl] = useState("");
+  const searchBtnPressed = function() {
+    setYturl(UrlInput.value);
   };
-
-	const addItemToData = (item) => {
-		let items = data["items"];
-
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(item),
-    };
-    
-    /* get - get info, post - send info, put - modify, delete - del */
-    fetch("http://localhost:3000/items", requestOptions)
-      .then((response) => response.json())
-      .then((data) => {
-        items.push(data);
-		    setData({ items: items });
-      });
-	};
-
-  const filterData = (data) => {
-    const filteredData = [];
-    
-    if (!filters.name) {
-      return data;
-    }
-    
-    for (const item of data) {
-      if (filters.name !== "" && item.name !== filters.name) {
-        continue;
-      }
-      if (filters.price !== "" && item.price !== filters.price) {
-        continue;
-      }
-      if (filters.type !== "" && item.type !== filters.type) {
-        continue;
-      }
-      if (filters.brand !== "" && item.brand !== filters.brand) {
-        continue;
-      }
-      
-      filteredData.push(item);
-    }
-
-    return filteredData;
-  };
+  console.log(yturl);
 
   return (
     <div className="App">
-      <div className="row mt-3">
-        <h2>Inventory Management Web App</h2>
-        <p>Store ur stuff here</p>
-      </div>
-      <div className="row mt-3">
-        <SearchBar updateSearchParams={updateFilters} />
-      </div>
-      <div className="row mt-3">
-        <AddItem addItem={addItemToData} />
-      </div>
-      <div className="row mt-3">
-          <ItemsDisplay
-            deleteItem={deleteItem}
-            items={filterData(data["items"])} />
-      </div>
+      <section className="header">
+        <div className="row">
+          <h2>Download from YouTube</h2>
+        </div>
+      </section>
+      <section className="content">
+        <div className="row">
+          <input
+            id="url-input"
+            type="text"
+            placeholder="Enter/Paste a valid URL here..."
+            value={yturl}
+          />
+        </div>
+        <div className="row">
+          <button type="button" onClick={searchBtnPressed}>
+            Download
+          </button>
+        </div>
+        <div className="row">
+          <img id="url-img" src="" alt=""></img>
+        </div>
+      </section>
+      <section className="footer">
+        <div className="row">
+        </div>
+      </section>
     </div>
   );
 }
