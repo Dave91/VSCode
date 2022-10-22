c0e4801b45374c42c2e3af92e84b5d72056abd37
Dave <thecothineal@gmail.com>
2022-10-22 20:20:04 +0200

    *

--- ReactApp/planetary/src/App.js
+++ ReactApp/planetary/src/App.js
@@ -21,19 +21,36 @@ function App() {
       let divobj = document.createElement("div");
       divobj.className = "objDiv";
       divobj.innerHTML = " ";
-      divobj.id = "d" + i;
-      divobj.style.left = 49 + i * 4 + "%";
-      divobj.style.top = 49 + i * 4 + "%";
+      divobj.style.left = 46 + i * 4 + "%";
+      divobj.style.top = 46 + i * 4 + "%";
       document.getElementById("objCont").appendChild(divobj);
     }
-    let divObjArray = document.getElementsByClassName("objDiv");
   }
 
   divObjCreate();
+  let divObjArray = document.getElementsByClassName("objDiv");
+  let delta = 0;
+
+  function divObjMove() {
+    requestAnimationFrame(divObjMove);
+    if (anim) {
+      for (let ii = 0; ii < divObjArray.length; ii++) {
+        let obj = divObjArray[ii];
+        obj.style.left = (anim) ? 46 + Math.sin(delta + ii) * (4 + 4 * ii) + "%" : 46 + ii * 4 + "%";
+        obj.style.top = (anim) ? 46 + Math.cos(delta + ii) * (1 + 1 * ii) + "%" : 46 + ii * 4 + "%";
+      }
+      delta = (anim) ? delta + 0.01 : 0;
+    }
+  }
+
+  divObjMove();
 
   return (
     <div>
       <div id="App">
+        <div id="objCont">
+        </div>
+
         <h2>Download from YouTube</h2>
         <div>
           <input
@@ -57,9 +74,6 @@ function App() {
         <button type="button" onClick={searchBtnPressed}>
           Download
         </button>
-
-        <div id="objCont">
-        </div>
 
         <div class="content" id="content1">
         </div>
