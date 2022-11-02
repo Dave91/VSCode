205893c76a627cebe55697d4d5f0e8a11917c85b
Dave <thecothineal@gmail.com>
2022-11-02 18:10:07 +0100

    *code fix, events, popup box, etc

--- threejs-solar-system/main.js
+++ threejs-solar-system/main.js
@@ -11,68 +11,19 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 const renderer = new THREE.WebGLRenderer();
-camera.position.set(6, 16, 25);
+camera.position.set(50, 50, 25);
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 renderer.render(scene, camera);
 
-// Raycaster (handles click events)
-
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-function renderRay() {
-  // mouse pos camera to pick object clicked
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
-  if (intersects[0].object.name) {
-    let answer = window.confirm("Do you wish to open link on new tab?");
-    if (answer === false) {
-      return;
-    }
-  }
-
-  // create lists for objects and links
-  // get index of intersects[0].object.name in objects list
-  // get the link of that same index from links list
-  // window.open("link from above");
-
-  if (intersects[0].object.name === "moon") {
-    window.open("https://en.wikipedia.org/wiki/Moon");
-  }
-  if (intersects[0].object.name === "earth") {
-    window.open("https://www.google.hu/intl/hu/earth/");
-  }
-  if (intersects[0].object.name === "mars") {
-    window.open("https://en.wikipedia.org/wiki/Mars");
-  }
-  if (intersects[0].object.name === "sun") {
-    window.open("https://en.wikipedia.org/wiki/Sun");
-  }
-  renderer.render(scene, camera);
-}
-
-// Click Events
-
-function onMouseClick(event) {
-  // calc mouse pos in normalized device coords
-  // (-1 to +1)
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-  window.requestAnimationFrame(renderRay);
-}
-
-// window.addEventListener('click', onMouseClick);
-
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 2);
-pointLight.position.set(-25, 40, -30);
+const pointLight = new THREE.PointLight(0xffffff, 1.4);
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-
 scene.add(pointLight, ambientLight);
+pointLight.position.set(0, 0, 0);
 
 // Background
 
@@ -92,17 +43,16 @@ const uranusDia = N / 27.2;
 const neptuneDia = N / 28.1;
 
 const scaleDown = 100;
-// for planet/moon distances, too?
 
 // distances from sun, pos(x)
-const mercuryDist = (N * 41.6) / scaleDown;
-const venusDist = (N * 77.7) / scaleDown;
-const earthDist = (N * 107.4) / scaleDown;
-const marsDist = (N * 163.7) / scaleDown;
-const jupiterDist = (N * 559) / scaleDown;
-const saturnDist = (N * 1030) / scaleDown;
-const uranusDist = (N * 2061) / scaleDown;
-const neptuneDist = (N * 3230) / scaleDown;
+const mercuryDist = (N * 41.6) / scaleDown + N / 2;
+const venusDist = (N * 77.7) / scaleDown + N / 2;
+const earthDist = (N * 107.4) / scaleDown + N / 2;
+const marsDist = (N * 163.7) / scaleDown + N / 2;
+const jupiterDist = (N * 559) / scaleDown + N / 2;
+const saturnDist = (N * 1030) / scaleDown + N / 2;
+const uranusDist = (N * 2061) / scaleDown + N / 2;
+const neptuneDist = (N * 3230) / scaleDown + N / 2;
 
 function createObj(objText, objNorm, objDia, objName, objXYZ) {
   let obj = new THREE.Mesh(
@@ -119,13 +69,7 @@ function createObj(objText, objNorm, objDia, objName, objXYZ) {
 }
 
 // Sun
-const sun = createObj(
-  "./images/sun.jpg",
-  "./images/sun.jpg",
-  N,
-  "sun",
-  [-60, 0, 0]
-);
+const sun = createObj("./images/sun.jpg", "", N / 2, "sun", [0, 0, 0]);
 
 // Planets
 const mercury = createObj(
@@ -210,20 +154,76 @@ const deimos = createObj(
   [marsDist, 0, 10]
 );
 
+const infobox = document.getElementById("infobox");
+let animIsPaused = true;
+
+// Raycaster (handles click events)
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function renderRay() {
+  // mouse pos camera to pick object clicked
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (!intersects[0].object.name) {
+    return;
+  }
+
+  // create lists for objects and links
+  // get index of intersects[0].object.name in objects list
+  // get the link of that same index from links list
+  // window.open("link from above");
+
+  if (intersects[0].object.name === "moon") {
+    window.open("https://en.wikipedia.org/wiki/Moon");
+  }
+  if (intersects[0].object.name === "earth") {
+    window.open("https://www.google.hu/intl/hu/earth/");
+  }
+  if (intersects[0].object.name === "mars") {
+    window.open("https://en.wikipedia.org/wiki/Mars");
+  }
+  if (intersects[0].object.name === "sun") {
+    window.open("https://en.wikipedia.org/wiki/Sun");
+  }
+  renderer.render(scene, camera);
+}
+
+// Click Events
+
+window.addEventListener("click", function (ev) {
+  // calc mouse pos in normalized device coords
+  // (-1 to +1)
+  mouse.x = (ev.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(ev.clientY / window.innerHeight) * 2 + 1;
+  //camera.position.set(earth.position.x, 5, 5);
+  //window.requestAnimationFrame(renderRay);
+});
+
+// Key Press Events
+
+window.addEventListener("keydown", function (ev) {
+  if (ev.code != "Space") {
+    return;
+  }
+  animIsPaused = animIsPaused ? false : true;
+  infobox.style.display = animIsPaused ? "grid" : "none";
+});
+
 // Window Resize Event
 
-function onWindResize() {
+window.addEventListener("resize", function () {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
-}
-
-window.addEventListener("resize", onWindResize);
+});
 
 // Controls, helpers
+
 const axisHelp = new THREE.AxesHelper(50);
-const gridHelp = new THREE.GridHelper(100, 20);
+const gridHelp = new THREE.PolarGridHelper(1000, 10, 10);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 scene.add(axisHelp, gridHelp);
@@ -231,6 +231,16 @@ scene.add(axisHelp, gridHelp);
 // Object Lists
 
 let planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+let planetDists = [
+  mercuryDist,
+  venusDist,
+  earthDist,
+  marsDist,
+  jupiterDist,
+  saturnDist,
+  uranusDist,
+  neptuneDist,
+];
 //moons = [[], [], [moon], [phobos, deimos]];
 
 // Animation Loop
@@ -241,29 +251,33 @@ function animate() {
   requestAnimationFrame(animate);
 
   sun.rotation.y -= 0.001;
-
-  // planets movement
-  for (let i = 0; i < planets.length; i++) {
-    let p = planets[i];
-    p.position.x = sun.position.x + Math.sin(delta + i * 0.2) * (25 + i * 10);
-    p.position.z = sun.position.z + Math.cos(delta + i * 0.2) * (30 + i * 10);
+  for (let p of planets) {
     p.rotation.y += 0.01;
   }
 
-  // moons movement
-  moon.position.x = earth.position.x - Math.sin(delta + 1) * 4;
-  moon.position.z = earth.position.z - Math.cos(delta + 1) * 5;
-  moon.rotation.y += 0.01;
+  if (!animIsPaused) {
+    // planets movement
+    for (let i = 0; i < planets.length; i++) {
+      let p = planets[i];
+      p.position.x = sun.position.x + Math.sin(delta + i * 0.2) * planetDists[i];
+      p.position.z = sun.position.z + Math.cos(delta + i * 0.2) * (planetDists[i] + 5);
+    }
 
-  phobos.position.x = mars.position.x - Math.sin(delta + 1) * 4;
-  phobos.position.z = mars.position.z - Math.cos(delta + 1) * 5;
-  phobos.rotation.y += 0.01;
+    // moons movement
+    moon.position.x = earth.position.x - Math.sin(delta + 1) * 3;
+    moon.position.z = earth.position.z - Math.cos(delta + 1) * 4;
+    moon.rotation.y += 0.01;
 
-  deimos.position.x = mars.position.x - Math.sin(delta + 1) * 6;
-  deimos.position.z = mars.position.z - Math.cos(delta + 1) * 7;
-  deimos.rotation.y += 0.01;
+    phobos.position.x = mars.position.x - Math.sin(delta + 1) * 2;
+    phobos.position.z = mars.position.z - Math.cos(delta + 1) * 3;
+    phobos.rotation.y += 0.01;
 
-  delta += 0.005;
+    deimos.position.x = mars.position.x - Math.sin(delta + 1) * 4;
+    deimos.position.z = mars.position.z - Math.cos(delta + 1) * 5;
+    deimos.rotation.y += 0.01;
+
+    delta += 0.005;
+  }
 
   renderer.render(scene, camera);
 }
