421e946be7f0537c0cfe9ab60f20517b70ce7ea8
Dave91 <thecothineal@gmail.com>
2026-01-29 17:58:35 +0100

    threejs cubes menu: *fixed menu display handling

--- threejs cubes menu/main.js
+++ threejs cubes menu/main.js
@@ -24,57 +24,16 @@ camera.position.set(0, 0, 35);
 
 // Lights
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
 scene.add(ambientLight);
-const pointLight = new THREE.PointLight(0xffffff, 0.8);
-pointLight.position.set(25, 50, 25); //10,10,10
+const pointLight = new THREE.PointLight(0xffffff, 0.6);
+pointLight.position.set(10, 35, 10);
 scene.add(pointLight);
 
 // raycaster setup
 
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
-
-function renderRay() {
-  // mouse pos camera to pick object clicked
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
-  let sect = null;
-  if (intersects[0].object.userData.id === "rolam") {
-    sect = "rolam";
-  }
-  if (intersects[0].object.userData.id === "versek") {
-    sect = "versek";
-  }
-  if (intersects[0].object.userData.id === "irasok") {
-    sect = "irasok";
-  }
-  menu_anim = "out";
-  document.getElementById(sect).className = "";
-  renderer.render(scene, camera);
-  controls.autoRotate = false;
-}
-
-function handleClickEvent(event) {
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-  const intersects = raycaster.intersectObjects(scene.children);
-
-  if (intersects.length > 0) {
-    const object = intersects[0].object;
-    const sectionId = object.userData.id;
-
-    if (sectionId) {
-      console.log("Kiválasztva:", sectionId);
-      // Itt hívhatod meg az animációdat vagy az oldalváltást
-      handleMenuTransition(sectionId);
-    }
-  }
-}
-
-let menuAnim = "off";
 
 // Menu elements
 
@@ -114,57 +73,96 @@ const menuItems = [
       size: 2,
       height: 0.5,
       curveSegments: 6,
-    });
-    const labelTexture = new THREE.TextureLoader().load("./space.jpg");
-    const material = new THREE.MeshBasicMaterial({ map: labelTexture });
-    const mLabel = new THREE.Mesh(textObj, material);
-    mLabel.name = id;
-    mLabel.position.x = x;
-    mLabel.position.y = y;
-    mLabel.position.z = z;
-    scene.add(mLabel);
-  });
-} */
+      });
+      const labelTexture = new THREE.TextureLoader().load("./space.jpg");
+      const material = new THREE.MeshBasicMaterial({ map: labelTexture });
+      const mLabel = new THREE.Mesh(textObj, material);
+      mLabel.name = id;
+      mLabel.position.x = x;
+      mLabel.position.y = y;
+      mLabel.position.z = z;
+      scene.add(mLabel);
+      });
+      } */
 
-function handleMenuTransition(id) {
-  menuAnim = menuAnim === "off" ? "in" : "out";
-  const target = document.getElementById(id);
-  if (target) {
-    target.className = "";
-    controls.autoRotate = false;
-    menuAnim = "out";
+// Click event
+
+let contentVisible = false;
+let menuVisible = true;
+const sections = ["rolam", "versek", "irasok"];
+
+function onMouseHover(event) {
+  if (contentVisible) return;
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length > 0) {
+    const object = intersects[0].object;
+    object.scale.set(1.3, 1.3, 1.3);
+    object.rotation.x = 0;
+    object.rotation.y = 0;
+    object.rotation.z = 0;
+  } else {
+    menuItems.forEach((item) => {
+      item.scale.set(1, 1, 1);
+    });
   }
 }
 
-window.addEventListener("click", handleClickEvent, false);
+window.addEventListener("mousemove", onMouseHover);
+
+function onMouseClick(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length > 0) {
+    const object = intersects[0].object;
+    const sectionId = object.userData.id;
+    const target = document.getElementById(sectionId);
+    if (!contentVisible && menuVisible) {
+      target.className = "";
+      contentVisible = true;
+      menuVisible = false;
+    } else {
+      sections.forEach((sect) => {
+        document.getElementById(sect).className = "hidden";
+      });
+      contentVisible = false;
+      menuVisible = true;
+    }
+    return;
+  }
+  if (contentVisible && !menuVisible) {
+    sections.forEach((sect) => {
+      document.getElementById(sect).className = "hidden";
+    });
+    contentVisible = false;
+    menuVisible = true;
+  }
+}
+
+window.addEventListener("click", onMouseClick);
 
 // Controls setup
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-//controls.dampingFactor = 0.05;
+controls.dampingFactor = 0.5;
 controls.autoRotate = false;
 //controls.autoRotateSpeed = 0.5;
+
 // Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
   menuItems.forEach((item) => {
-    item.rotation.x += 0.001;
-    item.rotation.y += 0.001;
-    item.rotation.z += 0.001;
+    item.rotation.x += 0.0003;
+    item.rotation.y += 0.0003;
+    item.rotation.z += 0.0005;
   });
-  if (menuAnim === "in") {
-    if (camera.rotation.y < 0) {
-      camera.rotation.y += 0.05;
-    }
-  }
-  if (menuAnim === "out") {
-    if (camera.rotation.x < 1) {
-      camera.rotation.x += 0.05;
-    }
-  }
   renderer.render(scene, camera);
 }
 
